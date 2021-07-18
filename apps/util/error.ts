3966e71f15e23d540b0e2c58df074ba37a660b49
Septiyan Andika <septiyan.andika@gmail.com>
2021-07-18 13:20:55 +0700

    error handle from bigchaindb

--- apps/util/error.ts
+++ apps/util/error.ts
@@ -4,8 +4,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 export function wrapHandlerError(handler: (req: NextApiRequest, res: NextApiResponse<any>) => Promise<any>) {
     return async ( req: NextApiRequest, res:NextApiResponse) => {
       return handler(req, res)
-        .catch((error:Error) => {
-          console.error(error);
+        .catch((error:any) => { 
+          // error from bigchaindb
+          if(error.status && error.requestURI){
+            const status =error.status.split(" ")[0]||500;
+            return res.status(status).send({
+              error:error.message || error,
+              requestURI:error.requestURI
+            });
+          }
           return res.status(500).send({error:error.message || error});
         });
     }
